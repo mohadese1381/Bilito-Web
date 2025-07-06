137852ac9b3f8152e72eba656ba066f7ca78dc1f
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2025-07-06 19:23:01 +0330

    update it to main frame

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// لود کردن هدر و فوتر از فایل جدا
+fetch("components/header.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("header").innerHTML = data);
+
+fetch("components/footer.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("footer").innerHTML = data);
